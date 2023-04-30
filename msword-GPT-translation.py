b4d9db373752d2d3622dcaaa3fe03154f4e41c22
lancer1911 <39460817+lancer1911@users.noreply.github.com>
2023-04-30 22:08:29 +0800

    Add files via upload

---
+++ msword-GPT-translation.py
@@ -0,0 +1,118 @@
+import openai
+import docx
+import requests
+import json
+from configparser import ConfigParser
+import time
+import re
+import argparse
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+config = ConfigParser()
+config.read("settings.cfg")
+
+api_key = config.get("OpenAI", "api_key")
+model = config.get("OpenAI", "model")
+temperature = 0.2 
+
+LANGUAGES = {
+    "en": "English",
+    "fr": "French",
+    "de": "German",
+    "jp": "Japanese",
+    "es": "Spanish",
+    "zh": "Simplified Chinese"
+    # Add more languages if needed
+}
+
+
+def translate_text(text, source_lang, target_lang, custom_prompt, model, temperature):
+    openai.api_key = api_key
+    URL = config.get("ENDPOINTS", "URL")
+
+    payload = {
+        "model": model,
+        "temperature": temperature,
+        "messages": [
+            {"role": "system", "content": f"You are a helpful assistant that translates {LANGUAGES[source_lang]} text to {LANGUAGES[target_lang]}."},
+            {"role": "user", "content": f"Translate the following {LANGUAGES[source_lang]} text to {LANGUAGES[target_lang]}: '{text}'"},
+        ],
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai.api_key}",
+    }
+
+    response = requests.post(URL, headers=headers, json=payload)
+    response = response.json()
+
+    print("\n\033[32mAPI response:\033[0m", response)
+
+    if "error" in response and "Rate limit reached" in response["error"]["message"]:
+        wait_time_str = response["error"]["message"].split("Please try again in ")[1].split(".")[0]
+        wait_time = int(wait_time_str.rstrip("s"))
+        print(f"\n\033[33mRate limit reached. Waiting for {wait_time} seconds.\033[0m")
+        time.sleep(wait_time)
+        return translate_text(text, source_lang, target_lang, custom_prompt, model, temperature)
+
+    return response["choices"][0]["message"]["content"]
+
+def translate_docx(input_path, output_path, source_lang, target_lang, custom_prompt, model, temperature):
+    doc = docx.Document(input_path)
+    translated_doc = docx.Document()
+
+    # Set the default font to '宋体'
+    font = translated_doc.styles['Normal'].font
+    font.name = 'STSong'
+
+    for paragraph in doc.paragraphs:
+        text = paragraph.text
+        if text:
+            if paragraph.style.name.startswith('Heading'):
+                # Preserve heading levels
+                level = int(re.search(r'\d+', paragraph.style.name).group())
+                translated_text = translate_text(text, source_lang, target_lang, custom_prompt, model, temperature)
+                new_heading = translated_doc.add_heading(level=level)
+                new_heading.text = translated_text
+
+                # Set the font for heading to '宋体'
+                new_heading.style.font.name = 'STSong'
+            else:
+                translated_text = translate_text(text, source_lang, target_lang, custom_prompt, model, temperature)
+                new_para = translated_doc.add_paragraph(translated_text, style=paragraph.style)
+
+                # Set the font for paragraph to '宋体'
+                new_para.style.font.name = 'STSong'
+        else:
+            translated_doc.add_paragraph()
+
+    translated_doc.save(output_path)
+
+def get_input_file():
+    parser = argparse.ArgumentParser(description="Translate a Word document using GPT.")
+    parser.add_argument("input_file", nargs="?", help="Input .docx file to be translated")
+    args = parser.parse_args()
+
+    if args.input_file:
+        return args.input_file
+    else:
+        root = tk.Tk()
+        root.withdraw()
+        return filedialog.askopenfilename(filetypes=[("Word documents", "*.docx")])
+
+def get_output_file(input_path):
+    file_name, file_ext = os.path.splitext(input_path)
+    return f"{file_name}_translated{file_ext}"
+
+
+if __name__ == "__main__":
+    input_path = get_input_file() 
+    output_path = get_output_file(input_path) 
+    source_lang = config.get("Prompts", "source_lang")
+    target_lang = config.get("Prompts", "target_lang")
+    custom_prompt = config.get("Prompts", "custom_prompt")
+
+    translate_docx(input_path, output_path, source_lang, target_lang, custom_prompt, model, temperature)
---
+++ readme-zh.md
@@ -0,0 +1,48 @@
+# msword-GPT-translation
+
+[English](./readme.md)
+
+`msword-GPT-translation` 是一个使用 ChatGPT API 或 GPT-4 API 进行翻译的简单工具，可以实现 Microsoft Word 文档（`.docx` 格式）的翻译。它可以保留原始文档中的格式，并允许用户自定义字体。本工具适用于需要将 Word 文档翻译成其他语言的用户。
+
+## 安装
+
+首先，确保您已经安装了 Python 3。然后，在终端中运行以下命令：
+
+```bash
+pip install -r requirements.txt
+```
+
+这将安装本工具所需的所有依赖。
+
+## 配置
+
+在使用本工具之前，您需要创建一个名为 `settings.cfg` 的配置文件。您可以从 `settings.cfg.template` 文件开始，复制并重命名为 `settings.cfg`。然后，将您的 OpenAI API 密钥添加到配置文件中的相应位置。
+
+```
+[openai]
+api_key = sk-
+```
+
+此外，您可以根据需要自定义其他设置，如源语言、目标语言等。
+
+## 使用
+
+要使用 `msword-GPT-translation`，只需运行以下命令：
+
+```bash
+python msword-GPT-translation.py sample.docx
+```
+
+其中，`sample.docx` 是您需要翻译的 Word 文档。
+
+如果您没有在命令行中指定输入文件，程序会弹出一个文件选择对话框，让您浏览并选择需要翻译的文件。
+
+翻译完成后，结果将保存在与输入文件同一目录下，文件名为 "原文件名-translated.docx"。
+
+## 注意
+
+由于 OpenAI API 有速率限制，请注意不要过快地连续发起请求。如果遇到速率限制错误，程序会自动等待一段时间后重试。请确保您已经在您的 OpenAI 账户中设置了合适的速率限制。
+
+## 示例
+
+在本仓库中，我们提供了一个名为 `sample.docx` 的样本 Word 文档。您可以使用此文件来测试 `msword-GPT-translation` 工具。
